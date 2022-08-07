53b460a1171acbcd784c345d07391b227bf40643
Сергей <99472918+Stalexus@users.noreply.github.com>
2022-08-07 13:21:44 +0300

    init(budget-app): add Badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+
 export const Badge = () => {
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
+  const handleEdit = () => {
+    setIsEditMode(true);
+  };
+
+  const handleSave = () => {
+    setIsEditMode(false);
+  };
+
   return (
-    <div>Badge</div>
+    <div>
+      {isEditMode ? (
+        <>
+          <button onClick={handleSave}>Save</button>
+          <input />
+        </>
+      ) : (
+        <>
+          <button onClick={handleEdit}>Edit</button>
+          <p>3000$</p>
+        </>)}
+    </div>
   )
 }
